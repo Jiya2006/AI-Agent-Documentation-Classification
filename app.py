35a3c1515588ac4e6248d91afe276f03a10c5cf5
Jiya Jitendrasingh Baghel <jiyab623@gmail.com>
2026-09-22 00:10:58 +0530

    Prepare app for deployment

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 import gradio as gr
 
 from document_processor import extract_text
@@ -80,4 +81,8 @@ interface = gr.Interface(
 )
 
 
-interface.launch(share=True)
+interface.launch(server_name="0.0.0.0", server_port=7860)
+interface.launch(
+    server_name="0.0.0.0",
+    server_port=int(os.environ.get("PORT", 7860))
+)
